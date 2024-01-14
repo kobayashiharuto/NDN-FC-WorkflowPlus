8474f6933f358abb81ef25a90ab93bbab7dda7b3
Kobayashi Haruto <51015143+kobayashiharuto@users.noreply.github.com>
2024-01-14 13:50:51 +0900

    Update node_a_start.sh

--- work/NDN-original/sh_local/node_a_start.sh
+++ work/NDN-original/sh_local/node_a_start.sh
@@ -3,12 +3,12 @@ nfdc face create udp://172.22.0.6
 nfdc route add prefix /X nexthop udp://172.22.0.6
 # Yに貼る
 nfdc face create udp://172.22.0.6
-nfdc route add prefix /X nexthop udp://172.22.0.6
+nfdc route add prefix /Y nexthop udp://172.22.0.6
 # Zに貼る
 nfdc face create udp://172.22.0.6
-nfdc route add prefix /X nexthop udp://172.22.0.6
+nfdc route add prefix /Z nexthop udp://172.22.0.6
 # Wに貼る
 nfdc face create udp://172.22.0.6
-nfdc route add prefix /X nexthop udp://172.22.0.6
+nfdc route add prefix /W nexthop udp://172.22.0.6
 
 /work/NDN-original/ndn-cxx/build/examples/node_a_producer
